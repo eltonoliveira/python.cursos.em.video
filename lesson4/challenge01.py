906a323c3c4ebfe5a19d580985c7d6cdd4ebf052
Elton Oliveira <elton.douglas.oliv@gmail.com>
2018-02-14 02:18:19 -0200

    Lesson 4 done

---
+++ lesson4/challenge01.py
@@ -0,0 +1,4 @@
+# Create a script which reads the person's 
+# name and shows a welcome greeting by the value typed
+name = input('Hi, what is your name?: ')
+print('Hi, ', name, '! Nice to meet You!')
